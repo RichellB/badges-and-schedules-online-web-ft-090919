1289d04acdc9af67a40024837ab7fb4b330a3b5e
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-14 14:37:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(speakers)
-  room = []
-  #room.length = 7
+  
   room_array = []
   speakers.each_with_index do | name, index |
     room_message = "Hello, #{name}! You'll be assigned to room #{room[index]}!"
