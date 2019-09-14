cc527bd14da3192ad8002170da63bc6ba0aff920
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-14 14:09:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,4 +4,12 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  new_array = []
+  badge_message = ""
   
+  array.each do | name |
+    badge_message = "Hello, my name is #{name}"
+    new_array << badge_message
+  end
+  new_array
+end
